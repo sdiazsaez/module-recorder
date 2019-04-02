f32c95bd8ccb7c39402a0e3c49214e5186a8c2e9
simon <sdiaz.sz@gmail.com>
2019-04-02 04:43:33 -0300

    Service abstract action add arguments

--- tests/ModuleRegisterTest.php
+++ tests/ModuleRegisterTest.php
@@ -56,6 +56,7 @@ class ModuleRegisterTest extends TestCase {
                        ->registerService(SampleParentService::class);
 
         $provider = $this->getProvider()->addService(SampleChildService::class);
+
         $service = ModuleRequest::getService($this->getModuleName(), $this->getProviderName(), 'sampleParentService');
         $this->assertTrue($service->action() === 'action');
     }
